349c42bf3c32f1ca6584c8c7bd5ea46eb64d90fb
Kaspar Daemen <kaspar.daemen@gmail.com>
2015-06-16 18:56:40 +0200

    Opmaak 1.0

--- js/App.js
+++ js/App.js
@@ -1,30 +1,30 @@
 $(document).ready(function(){
 	var service = new BattleshipService();
-	var gameBoard = '#gameboard #locations';
+	var gameBoard = '.gameboard .locations';
 	var coords = [];
 	var currentGame;
-	
-	// Get My games and fill	
+
+	// Get My games and fill
 	var populateGames = function() {
-		
+
 		service.getGames(function(games){
-			
+
 			if(games.length > 0) {
 				$('#gamelist').empty();
 				$.each(games, function(index, game){
-					$('#gamelist').append('<li>' + game.enemyName + ' ' + game.status +'</li>');
+					$('#gamelist').append('<div>' + game.enemyName + ' ' + game.status +'</div>');
 				});
 			} else {
-				$('#gamelist').html('<li>Geen spellen</li>');
+				$('#gamelist').html('<div>Geen spellen</div>');
 			}
-			
-		});	
-		
+
+		});
+
 	};
-	
+
 	// Create coordinates
 	for(var i = 10; i > 0; i--) {
-		
+
 		for(var x = 0; x < 10; x++) {
 			coords.push({
 				'x' : String.fromCharCode(65 + x),
@@ -32,36 +32,36 @@ $(document).ready(function(){
 			);
 		}
 	}
-	
+
 	// Put events on buttons
 	$('#newGamePlayer').on('click', function(e){
 		service.createPlayerGame(populateGames);
 	});
-	
+
 	$('#newGameComputer').on('click', function(e){
 		service.createComputerGame(populateGames);
 	});
-	
+
 	// Generate grid
 	$.each(coords, function(index, value){
 		var cell = '<div class="cell" data-x="'+ value.x + '" data-y="'+ value.y + '"></div>';
 		$(gameBoard).append(cell);
 	});
-	
+
 	// Get ships
 	service.getShips(function (result) {
-		
+
 		$.each(result, function (index, value) {
 			$('#ships .panel-body').append('<div>' + value.name + '</div>');
 		});
-		
+
 	});
-	
+
 	populateGames();
-	
+
 	// Add hit or miss class to cell
 	var cells = $('.cell');
 	$(cells[40]).addClass('missed');
 	$(cells[75]).addClass('hit');
-	
+
 });
